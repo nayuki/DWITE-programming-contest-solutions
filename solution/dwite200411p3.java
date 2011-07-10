a384f2100d45a2b234bd6715c096cdc666442dae
Nayuki Minase <nayuki@eigenstate.org>
2011-07-10 16:45:49 +0000

    DWITE 2004-11 P3: Revamped core algorithms.

--- solution/dwite200411p3.java
+++ solution/dwite200411p3.java
@@ -5,7 +5,6 @@ import dwite.*;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import java.util.StringTokenizer;
 
 
 public final class dwite200411p3 extends Solution {
@@ -17,17 +16,17 @@ public final class dwite200411p3 extends Solution {
 	
 	protected void runOnce() {
 		// Read input
-		Polynomial poly = parsePolynomial(io.readLine()).reduce();
+		Polynomial poly = readPolynomial(io);
 		
 		// Find factors
 		ArrayList<Integer> output = new ArrayList<Integer>();
 		int a0 = poly.getCoefficient(poly.getDegree());
 		while (true) {
-			Fraction root = findRoot(poly);
+			int[] root = findRoot(poly);
 			if (root == null)
 				break;
-			output.add(a0 / root.denom * root.num);
-			poly = poly.divide(root.denom, -root.num);
+			output.add(a0 / root[0] * root[1]);
+			poly = poly.divide(root[0], root[1]);
 		}
 		
 		// Sort ascending and write output
@@ -42,29 +41,28 @@ public final class dwite200411p3 extends Solution {
 	}
 	
 	
-	private static Polynomial parsePolynomial(String line) {
-		StringTokenizer st = new StringTokenizer(line, " ");
-		int degree = Integer.parseInt(st.nextToken());
+	private static Polynomial readPolynomial(Io io) {
+		io.tokenizeLine();
+		int degree = io.readIntToken();
 		List<Integer> coef = new ArrayList<Integer>();
-		for (int i = 0; i < degree+1; i++)
-			coef.add(Integer.parseInt(st.nextToken()));
+		for (int i = 0; i < degree + 1; i++)
+			coef.add(io.readIntToken());
+		Collections.reverse(coef);
 		return new Polynomial(coef);
 	}
 	
 	
-	private static Fraction findRoot(Polynomial poly) {
+	private static int[] findRoot(Polynomial poly) {
 		int p = Math.abs(poly.getCoefficient(0));
 		int q = Math.abs(poly.getCoefficient(poly.getDegree()));
 		for (int i = 1; i <= p; i++) {
-			if (p % i != 0)
-				continue;
-			for (int j = 1; j <= q; j++) {
-				if (q % j != 0)
-					continue;
-				if (poly.hasRootAt(new Fraction( i, j)))
-					return new Fraction( i, j);
-				if (poly.hasRootAt(new Fraction(-i, j)))
-					return new Fraction(-i, j);
+			if (p % i == 0) {  // Find a factor of p
+				for (int j = 1; j <= q; j++) {
+					if (q % j == 0) {  // Find a factor of q
+						if (poly.hasRootAt( j, i)) return new int[]{ j, i};
+						if (poly.hasRootAt(-j, i)) return new int[]{-j, i};
+					}
+				}
 			}
 		}
 		return null;
@@ -74,7 +72,7 @@ public final class dwite200411p3 extends Solution {
 	
 	private static class Polynomial {
 		
-		private final List<Integer> coefficients;  // From highest power downward
+		private final List<Integer> coefficients;  // From lowest power upward
 		
 		
 		
@@ -91,56 +89,33 @@ public final class dwite200411p3 extends Solution {
 		
 		// Returns the coefficient of the monomial with the specified power.
 		public int getCoefficient(int i) {
-			return coefficients.get(coefficients.size() - 1 - i);
+			return coefficients.get(i);
 		}
 		
 		
-		// Returns this polynomial divided by the GCD of all coefficients.
-		public Polynomial reduce() {
-			int gcd = coefficients.get(0);
-			for (int a : coefficients) {
-				gcd = Algorithm.gcd(a, gcd);
-			}
-			
-			if (gcd == 1)
-				return this;
-			else {
-				List<Integer> coef = new ArrayList<Integer>();
-				for (int a : coefficients)
-					coef.add(a / gcd);
-				return new Polynomial(coef);
-			}
-			
+		public boolean hasRootAt(int c, int d) {
+			return divide(c, d) != null;
 		}
 		
 		
-		public Fraction evaluateAt(Fraction x) {
-			Fraction result = new Fraction(0);
-			for (int i = 0; i < coefficients.size(); i++) {
-				result = result.multiply(x);
-				result = result.add(new Fraction(coefficients.get(i)));
-			}
-			return result;
-		}
-		
-		
-		public boolean hasRootAt(Fraction x) {
-			return evaluateAt(x).isZero();
-		}
-		
-		
-		// Returns a new polynomial representing this polynomial divided by (ax + b).
-		public Polynomial divide(int a, int b) {
-			if (!hasRootAt(new Fraction(-b, a)))
-				throw new IllegalArgumentException();
+		// Returns a new polynomial representing this polynomial divided by (cx - d).
+		public Polynomial divide(int c, int d) {
 			List<Integer> coef = new ArrayList<Integer>();
 			int remainder = 0;
 			for (int i = getDegree(); i >= 1; i--) {
-				int quotient = (getCoefficient(i) - remainder) / a;
+				remainder += getCoefficient(i);
+				if (remainder % c != 0)
+					return null;
+				int quotient = remainder / c;
 				coef.add(quotient);
-				remainder = quotient * b;
+				remainder = quotient * d;
 			}
-			return new Polynomial(coef);
+			
+			if (getCoefficient(0) + remainder == 0) {
+				Collections.reverse(coef);
+				return new Polynomial(coef);
+			} else
+				return null;
 		}
 		
 		
@@ -161,53 +136,6 @@ public final class dwite200411p3 extends Solution {
 				sb.append(Math.abs(getCoefficient(i))).append(" x^").append(i);
 			}
 			return sb.toString();
-		}
-		
-	}
-	
-	
-	
-	private static class Fraction {
-		
-		private int num;
-		private int denom;
-		
-		
-		
-		public Fraction(int num) {
-			this(num, 1);
-		}
-		
-		
-		public Fraction(int num, int denom) {
-			if (denom == 0)
-				throw new IllegalArgumentException("Zero denominator");
-			int gcd = Algorithm.gcd(num, denom);
-			num /= gcd;
-			denom /= gcd;
-			this.num = num;
-			this.denom = denom;
-		}
-		
-		
-		
-		public Fraction add(Fraction x) {
-			return new Fraction(num * x.denom + x.num * denom, denom * x.denom);
-		}
-		
-		
-		public Fraction multiply(Fraction x) {
-			return new Fraction(num * x.num, denom * x.denom);
-		}
-		
-		
-		public boolean isZero() {
-			return num == 0;
-		}
-		
-		
-		public String toString() {
-			return String.format("%d/%d", num, denom);
 		}
 		
 	}
