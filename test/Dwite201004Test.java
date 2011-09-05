f8be2f1417d08f3d04b2358d028037939f43a1ee
Nayuki Minase <nayuki@eigenstate.org>
2011-09-05 22:27:11 +0000

    DWITE 2010-04: Added problems, solutions, and tests.

---
+++ test/Dwite201004Test.java
@@ -0,0 +1,35 @@
+import org.junit.Test;
+
+
+public class Dwite201004Test {
+	
+	@Test
+	public void testProblem1() throws Exception {
+		DwiteTestUtils.test(dwite201004p1.class, "201004-DATA1.txt", "201004-OUT1.txt");
+	}
+	
+	
+	@Test
+	public void testProblem2() throws Exception {
+		DwiteTestUtils.test(dwite201004p2.class, "201004-DATA2.txt", "201004-OUT2.txt");
+	}
+	
+	
+	@Test
+	public void testProblem3() throws Exception {
+		DwiteTestUtils.test(dwite201004p3.class, "201004-DATA3.txt", "201004-OUT3.txt");
+	}
+	
+	
+	@Test
+	public void testProblem4() throws Exception {
+		DwiteTestUtils.test(dwite201004p4.class, "201004-DATA4.txt", "201004-OUT4.txt");
+	}
+	
+	
+	@Test
+	public void testProblem5() throws Exception {
+		DwiteTestUtils.test(dwite201004p5.class, "201004-DATA5.txt", "201004-OUT5.txt");
+	}
+	
+}
