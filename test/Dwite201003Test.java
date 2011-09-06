6ccedb58695b7c1428a08a079809c480e3c524e7
Nayuki Minase <nayuki@eigenstate.org>
2011-09-06 15:02:51 +0000

    DWITE 2010-03: Added problems, solutions, and tests. The official test data has an error in problem 2.

---
+++ test/Dwite201003Test.java
@@ -0,0 +1,35 @@
+import org.junit.Test;
+
+
+public class Dwite201003Test {
+	
+	@Test
+	public void testProblem1() throws Exception {
+		DwiteTestUtils.test(dwite201003p1.class, "201003-DATA1.txt", "201003-OUT1.txt");
+	}
+	
+	
+	@Test
+	public void testProblem2() throws Exception {
+		DwiteTestUtils.test(dwite201003p2.class, "201003-DATA2.txt", "201003-OUT2.txt");
+	}
+	
+	
+	@Test
+	public void testProblem3() throws Exception {
+		DwiteTestUtils.test(dwite201003p3.class, "201003-DATA3.txt", "201003-OUT3.txt");
+	}
+	
+	
+	@Test
+	public void testProblem4() throws Exception {
+		DwiteTestUtils.test(dwite201003p4.class, "201003-DATA4.txt", "201003-OUT4.txt");
+	}
+	
+	
+	@Test
+	public void testProblem5() throws Exception {
+		DwiteTestUtils.test(dwite201003p5.class, "201003-DATA5.txt", "201003-OUT5.txt");
+	}
+	
+}
