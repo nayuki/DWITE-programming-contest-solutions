c26fcf0cae312dc12232ff17efa72fe05f078731
Nayuki Minase <nayuki@eigenstate.org>
2013-11-11 05:57:22 +0000

    DWITE 2011-07 P3: Optimized code for clarity using a less elegant approach to combinatorics.

--- solution/dwite201107p3.java
+++ solution/dwite201107p3.java
@@ -23,41 +23,26 @@ public final class dwite201107p3 extends DwiteSolution {
 		io.tokenizeLine();
 		int a = io.readIntToken();
 		int b = io.readIntToken();
-		int x = io.readIntToken();
-		int y = io.readIntToken();
+		int c = io.readIntToken();
+		int d = io.readIntToken();
 		
 		// Triangles are similar iff they have the same multiset of side lengths
 		Set<List<Integer>> triangles = new HashSet<List<Integer>>();
-		int[] coordinates = new int[6];
-		for (int i = 0; i < coordinates.length; i += 2) {
-			coordinates[i + 0] = a;
-			coordinates[i + 1] = b;
-		}
-		while (true) {
+		for (int x0 = a; x0 <= c; x0++) {
+		for (int x1 = a; x1 <= c; x1++) {
+		for (int x2 = a; x2 <= c; x2++) {
+		for (int y0 = b; y0 <= d; y0++) {
+		for (int y1 = b; y1 <= d; y1++) {
+		for (int y2 = b; y2 <= d; y2++) {
 			Integer[] sides = {
-				distSqr(coordinates[0], coordinates[1], coordinates[2], coordinates[3]),
-				distSqr(coordinates[2], coordinates[3], coordinates[4], coordinates[5]),
-				distSqr(coordinates[4], coordinates[5], coordinates[0], coordinates[1]),
+				distSqr(x0, y0, x1, y1),
+				distSqr(x1, y1, x2, y2),
+				distSqr(x2, y2, x0, y0),
 			};
 			Arrays.sort(sides);
 			if (!(sides[0] == 0 || sqr(sides[2] - sides[0] - sides[1]) == sides[0] * sides[1] * 4))  // Exclude degenerate triangles
 				triangles.add(Arrays.asList(sides));
-			
-			// Increment coordinates
-			int i = coordinates.length - 1;
-			while (i >= 0) {
-				if (i % 2 == 0 && coordinates[i] == x)
-					coordinates[i] = a;
-				else if (i % 2 == 1 && coordinates[i] == y)
-					coordinates[i] = b;
-				else
-					break;
-				i--;
-			}
-			if (i < 0)
-				break;
-			coordinates[i]++;
-		}
+		}}}}}}
 		io.println(triangles.size());
 	}
 	
